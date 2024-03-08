c421048101ce3aee0f136bc0df40880a6a636397
poczemartin <pocze.martin@students.jedlik.eu>
2024-03-08 09:26:12 +0100

    changes

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ from menu import Menu
 from game import Game
 
 class App:
+
     def __init__(self) -> None:    
         self.main_loop = fusion.Window()
         self.manager = fusion.SceneManager()  
         self.menu = fusion.Scene("menu", Menu)
         self.game = fusion.Scene("game", Game)
-        self.manager.add_scene("menu")
-        self.manager.add_scene("game")
+        self.manager.add_scene(self.menu)
+        self.manager.add_scene(self.game)
 
         @self.main_loop.loop
         def loop():
             self.manager.start()
+            
