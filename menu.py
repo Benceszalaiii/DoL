016780967925388992558902c4dd1e0b37cbcc9b
Benceszalaiii <szbence2007@gmail.com>
2024-03-13 13:06:11 +0100

    white space

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ import fusionengine as fusion
 
 class Menu:
     menu_scene: fusion.Scene
+    
     def __init__(self):
         self.menu_scene = fusion.Scene("DoL", Game)
         self.menu_window = fusion.Window("DoL", 1280, 720)
