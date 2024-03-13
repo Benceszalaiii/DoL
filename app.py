1bdb8b8a0f1b4679db3965e154fd5b47979e2b18
Benceszalaiii <szbence2007@gmail.com>
2024-03-13 13:20:12 +0100

    Utáljuk a bogarakat (és a c1ö93c7uoe3uihehaoac hil

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import fusionengine as fusion
 from menu import Menu
 from game import Game
+from settings import FPS, WIDTH, HEIGHT, TITLE
 
 
 class App:
 
     def __init__(self) -> None:
-        self.main_loop = fusion.Window()
-        self.manager = fusion.SceneManager()
+        self.main_loop = fusion.Window(TITLE, WIDTH, HEIGHT)
+        self.main_loop.set_fps(FPS)
+        self.manager = fusion.SceneManager(self.main_loop)
         self.menu = fusion.Scene("menu", Menu)
         self.game = fusion.Scene("game", Game)
         self.manager.add_scene(self.menu)
