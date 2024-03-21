713cff1d23e620bc53e5a8d93c714554688b6372
hmate06 <horvath.mate3@students.jedlik.eu>
2024-03-21 12:36:47 +0100

    javitas

--- menu.py
+++ menu.py
@@ -4,8 +4,6 @@
 #  IMPORTS  #
 # --------- #
 
-
-
 import fusionengine as fusion
 from settings import TITLE, FPS, WIDTH, HEIGHT
 import pygame as pg
@@ -23,8 +21,6 @@ class Menu:
     # INITIALIZE SESSION  #
     # ------------------- #
 
-    play_button: fusion.Button = fusion.Button(533, 300, 533, 600, 50, "Play")
-
     def __init__(self, win):
         self.win = win
         self.win._running = True
@@ -34,12 +30,6 @@ class Menu:
         #self.settings_button = fusion.Button(533, 600, 533, 25, 50, "Settings")
 
         print("Welcome to DoL!")
-
-    # def play_button_load(self):
-    #    self.play_button: fusion.Button = fusion.Button(200, 200, 200, 75, 32, "Play")
-
-    # def settings_button_load(self):
-    #    self.settings_button: fusion.Button = fusion.Button(200, 200, 200, 75, 32, "Settings")
 
     # ------------------- #
     #    START SESSION    #
@@ -53,9 +43,6 @@ class Menu:
             self.background.load_image("menu_background.jpg")
             self.inputs()
             self.background.update()
-            #self.settings_button.draw()
-            self.play_button.draw()
-            # self.settings_button.draw()
 
 # <---- END OF GAME LOOP
 
