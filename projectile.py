bd085d65f416c6513bd00e4e2ae23e604a07f537
poczemartin <pocze.martin@students.jedlik.eu>
2024-03-22 09:19:04 +0100

    hosszú sor átírva

--- projectile.py
+++ projectile.py
@@ -11,14 +11,22 @@ class Projectile():
         #self.top_y: float = 0
         self.dx = dx
         self.dy = dy
+        self._one_to_four()
         #                         Fentrol jon                                                            Balrol jon                                                     Lentrol jon                                                                                 Jobbrol jon
-        self.proj_x, self.proj_y = (random.randint(0, WIDTH), 0) if bool(random.randint(0, 1)) else (0, random.randint(0, HEIGHT)) if bool(random.randint(0, 1))  else (random.randint(0, WIDTH), HEIGHT - 50) if bool(random.randint(0, 1)) else (WIDTH - 50, random.randint(0, HEIGHT))
+        #self.proj_x, self.proj_y = (random.randint(0, WIDTH), 0) if bool(random.randint(0, 1)) else (0, random.randint(0, HEIGHT)) if bool(random.randint(0, 1))  else (random.randint(0, WIDTH), HEIGHT - 50) if bool(random.randint(0, 1)) else (WIDTH - 50, random.randint(0, HEIGHT))
         self.top_proj: fe.Node = fe.Node(win, self.proj_x, self.proj_y, 50, 50)
         #self.top_proj: fe.Node = fe.Node(win, self.top_x, self.top_y, 50, 50)
 
-    #def _one_to_four(self) -> int:
-        #_chance: int = random.randint(0, 4)
-        #return _chance
+    def _one_to_four(self):
+        _chance: int = random.randint(1, 4)
+        if _chance == 1:
+            self.proj_x, self.proj_y = (random.randint(0, WIDTH), 0)
+        elif _chance == 2:
+            self.proj_x, self.proj_y = (0, random.randint(0, HEIGHT))
+        elif _chance == 3:
+            self.proj_x, self.proj_y = (random.randint(0, WIDTH), HEIGHT - 50)
+        else:
+            self.proj_x, self.proj_y = (WIDTH - 50, random.randint(0, HEIGHT))
 
     #def _move(self):
         #self.top_proj.x += proj_speed * self.dx
