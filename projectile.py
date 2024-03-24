4a37486163b1bcad34ef46ae1f867bb581e3e040
poczemartin <pocze.martin@students.jedlik.eu>
2024-03-24 22:20:43 +0100

    kis változások

--- projectile.py
+++ projectile.py
@@ -1,21 +1,24 @@
 import random
 import pygame
 import fusionengine as fe
-from settings import WIDTH, HEIGHT
+from settings import SPEED, WIDTH, HEIGHT
+from player import Player
 
-proj_speed = 8
-class Projectile():
+proj_speed = 0
+
+
+class Projectile:
 
     def __init__(self, win: fe.Window, dx: float, dy: float):
-        #self.top_x: float = random.randint(0, WIDTH)
-        #self.top_y: float = 0
+        # self.top_x: float = random.randint(0, WIDTH)
+        # self.top_y: float = 0
         self.dx = dx
         self.dy = dy
         self._one_to_four()
         #                         Fentrol jon                                                            Balrol jon                                                     Lentrol jon                                                                                 Jobbrol jon
-        #self.proj_x, self.proj_y = (random.randint(0, WIDTH), 0) if bool(random.randint(0, 1)) else (0, random.randint(0, HEIGHT)) if bool(random.randint(0, 1))  else (random.randint(0, WIDTH), HEIGHT - 50) if bool(random.randint(0, 1)) else (WIDTH - 50, random.randint(0, HEIGHT))
+        # self.proj_x, self.proj_y = (random.randint(0, WIDTH), 0) if bool(random.randint(0, 1)) else (0, random.randint(0, HEIGHT)) if bool(random.randint(0, 1))  else (random.randint(0, WIDTH), HEIGHT - 50) if bool(random.randint(0, 1)) else (WIDTH - 50, random.randint(0, HEIGHT))
         self.top_proj: fe.Node = fe.Node(win, self.proj_x, self.proj_y, 50, 50)
-        #self.top_proj: fe.Node = fe.Node(win, self.top_x, self.top_y, 50, 50)
+        # self.top_proj: fe.Node = fe.Node(win, self.top_x, self.top_y, 50, 50)
 
     def _one_to_four(self):
         _chance: int = random.randint(1, 4)
@@ -28,11 +31,13 @@ class Projectile():
         else:
             self.proj_x, self.proj_y = (WIDTH - 50, random.randint(0, HEIGHT))
 
-    #def _move(self):
-        #self.top_proj.x += proj_speed * self.dx
-        #self.top_proj.y += proj_speed * self.dy
+    def proj_speed(self):
+        player_x = SPEED * Player.dx * 3
 
     def update(self):
         self.top_proj.load_rect(fe.BLUE)
-        #self._move()
+        # self._move()
         self.top_proj.update()
+
+    def ellesznevezve(self):
+        pass
