6422f1c62a79bd320e294dba9f1399df5ad452c1
poczemartin <pocze.martin@students.jedlik.eu>
2024-03-30 18:46:22 +0100

    mozgatás demója készen van

--- projectile.py
+++ projectile.py
@@ -16,10 +16,10 @@ class Projectile:
         self.dx = dx
         self.dy = dy
         self._one_to_four()
-        self.all_bullets = []
+        # self.all_bullets = []
         #                         Fentrol jon                                                            Balrol jon                                                     Lentrol jon                                                                                 Jobbrol jon
         # self.proj_x, self.proj_y = (random.randint(0, WIDTH), 0) if bool(random.randint(0, 1)) else (0, random.randint(0, HEIGHT)) if bool(random.randint(0, 1))  else (random.randint(0, WIDTH), HEIGHT - 50) if bool(random.randint(0, 1)) else (WIDTH - 50, random.randint(0, HEIGHT))
-        self.top_proj: fe.Node = fe.Node(win, self.proj_x, self.proj_y, 50, 50)
+        self.proj: fe.Node = fe.Node(win, self.proj_x, self.proj_y, 50, 50)
         # self.top_proj: fe.Node = fe.Node(win, self.top_x, self.top_y, 50, 50)
 
     def _one_to_four(self):
@@ -34,32 +34,32 @@ class Projectile:
             self.proj_x, self.proj_y = (WIDTH - 50, random.randint(0, HEIGHT))
 
     def update(self):
-        self.top_proj.load_rect(fe.BLUE)
+        self.proj.load_rect(fe.BLUE)
         self.distance_math()
-        self.proj_x += self.speed_x
-        self.proj_y += self.speed_y
-        self.top_proj.update()
+        self.proj.x += self.speed_x
+        self.proj.y += self.speed_y
+        self.proj.update()
 
     def distance_math(self):
-        distance_x = self.position()[0] - self.proj_x
-        distance_y = self.position()[1] - self.proj_x
+        distance_x = 10 - self.proj_x
+        distance_y = 10 - self.proj_y
         angle = math.atan2(distance_y, distance_x)
-        speed_x = 5 * math.cos(angle)
-        speed_y = 5 * math.sin(angle)
-        self.all_bullets.append([self.proj_x, self.proj_y, speed_x, speed_y])
+        self.speed_x = 5 * math.cos(angle)
+        self.speed_y = 5 * math.sin(angle)
+        # self.all_bullets.append([self.proj_x, self.proj_y, self.speed_x, self.speed_y])
 
-    def position(self):
-        self.mouse_pos: list[int] = []
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
-                self.mouse_pos.append(self.mouse_x)
-                self.mouse_pos.append(self.mouse_y)
-                return self.mouse_pos
-        return self.mouse_pos
+    # def position(self):
+    # self.mouse_pos: list[int] = []
+    # for event in pygame.event.get():
+    # if event.type == pygame.MOUSEBUTTONDOWN:
+    # self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
+    # self.mouse_pos.append(self.mouse_x)
+    # self.mouse_pos.append(self.mouse_y)
+    # return self.mouse_pos
+    # return self.mouse_pos
 
-   # def list(self):
-       # for pos_x, pos_y, self.speed_x, self.speed_y in self.all_bullets:
-            #pos_x = int(pos_x)
-            #pos_y = int(pos_y)
 
+# def list(self):
+# for pos_x, pos_y, self.speed_x, self.speed_y in self.all_bullets:
+# pos_x = int(pos_x)
+# pos_y = int(pos_y)
