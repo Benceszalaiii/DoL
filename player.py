4f3ff49bd51a2fe0db0c05d6f923b9550f3e3b2b
poczemartin <pocze.martin@students.jedlik.eu>
2024-04-05 09:11:56 +0200

    player logika movement

--- player.py
+++ player.py
@@ -7,7 +7,7 @@
 import os
 import pygame as pg
 from settings import SPEED
-import math
+from math import atan2, cos, sin
 from utils import empty_terminal as clr
 print("Loading player")
 # ------------------- #
@@ -26,13 +26,16 @@ class Player():
         self.model = pg.transform.scale(self.model, (w, h))
         self.rect = self.model.get_rect()
         self.rect.topright = (x, y)
-        self.erect = pg.rect.Rect(x, y, w, h)
-        self.erect.center = (x, y)
         self.crect = pg.rect.Rect((self.rect.centerx - 3), (self.rect.centery - 3), self.rect.width/4, self.rect.height/4)
         self.dest_cord = (self.crect.size)
         self.dest = pg.Rect(self.crect.x, self.crect.y, 12, 12)
-        self.dx, self.dy = 0, 0
-
+        
+        self.movement = []
+        self.pos_x = 0
+        self.pos_y = 0
+        self.speed_x = 0
+        self.speed_y = 0
+        
     # ----------- #
     #   METHODS   #
     # ----------- #
@@ -45,14 +48,15 @@ class Player():
             self.move_player(pg.mouse.get_pos())
         # Move the character towards the target on each tick
         self.walk(delta)
-
-        # Update the character's rect to match the updated position
+                # Update the character's rect to match the updated position
         self.rect.update(self.crect.centerx - (self.rect.width/2), self.crect.centery - (self.rect.height/2), self.rect.width, self.rect.height)
+        
 
     def render(self, screen: pg.Surface):
         screen.blit(self.model, self.rect)
-        pg.draw.rect(screen, (255, 0, 255), self.erect)
         pg.draw.rect(screen, "red", self.dest)
+        pg.draw.rect(screen, (255, 0, 0), ((self.pos_x, self.pos_y), (30, 30)))
+
         """
         pg.draw.rect(screen, "green", self.crect)"""
     """    def walk(self, delta) -> None:
@@ -76,25 +80,16 @@ class Player():
         else:
             print("Collided")"""
     def move_player(self, target_pos):
-        dest_x, dest_y = target_pos
-        self.dest.center = (dest_x, dest_y)
+        mouse_x, mouse_y = target_pos
+        distance_x = mouse_x - self.pos_x
+        distance_y = mouse_y - self.pos_y
+        angle = atan2(distance_y, distance_x)
+        self.speed_x = SPEED * cos(angle)
+        self.speed_y = SPEED * sin(angle)
+        #self.movement.append([self.pos_x, self.pos_y, self.speed_x, self.speed_y])
 
 
-    def walk(self, delta):
-        if not self.erect.colliderect(self.dest):
-            dest_x, dest_y = self.dest.center
-            self.dx = dest_x - self.erect.x
-            self.dy = dest_y - self.erect.y
-            length = math.hypot(self.dx, self.dy)
-            if length != 0:
-                self.dx /= length
-                self.dy /= length
-            self.erect.centerx += SPEED * self.dx * delta
-            self.erect.centery += SPEED * self.dy * delta
-            clr()
-            print("Dest:", self.dest.center)
-            print("Rect:", self.erect.centerx, self.erect.centery)
-            print("xy diff: ", self.dx - self.dy)
-            print(SPEED * self.dx * delta, SPEED * self.dy * delta)
-        else:
-            print("Collided")
+    def walk(self, delta: float):
+        self.pos_x += self.speed_x * delta  # pos_x += speed_x
+        self.pos_y += self.speed_y * delta
+
