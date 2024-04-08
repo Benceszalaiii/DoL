0f8fdcfc1b6a479f515b4a526906e92110a3329f
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2024-04-08 12:34:42 +0200

    numpy megolese

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 from os import system as cmd
-import numpy as np
 
 DEBUGMODE = False
 # ----------- #
@@ -20,17 +19,3 @@ def clr() -> None:
 
 
 print("Utilities loaded")
-
-
-def sqrt(number: float):
-    threehalfs = 1.5
-    x2 = number * 0.5
-    y = np.float32(number)
-    i = y.view(np.int32)
-    i = np.int32(0x5F3759DF) - np.int32(i >> 1)  # what the fuck?
-    y = i.view(np.float32)
-    y = y * (threehalfs - (x2 * y * y))
-    return float(y)
-
-
-print(sqrt(16))
